79974548c124a983a033df0c805cd38685a593d0
caron.sim <caron.sim@kakaocorp.com>
2019-09-13 10:28:40 +0900

    week1 - LongestPalindrome

---
+++ leetcode/week1/LongestPalindrome.java
@@ -0,0 +1,35 @@
+package week1;
+
+public class LongestPalindrome {
+
+    public String longestPalindrome(String s) {
+        final int len = s.length();
+        int maxLen = 0;
+        String answer = "";
+        for (int i = 0; i < len; i++) {
+            for (int j = i; j < len; j++) {
+                if (isPalindrome(s.substring(i, j + 1))) {
+                    if (maxLen < j - i + 1) {
+                        maxLen = j - i + 1;
+                        answer = s.substring(i, j + 1);
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public boolean isPalindrome(String s) {
+        final int len = s.length();
+
+        for (int i = 0; i < len/2; i++) {
+            if (s.charAt(i) != s.charAt(len - i - 1)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
