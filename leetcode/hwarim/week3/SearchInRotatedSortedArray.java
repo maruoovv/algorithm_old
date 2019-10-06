d3169b111671b6e7f4190aca30e538dcdcf19db2
hwarim_laptop <rmrmhrm@gmail.com>
2019-10-06 15:46:53 +0900

    Update SearchInRotatedSortedArray.java

--- leetcode/hwarim/week3/SearchInRotatedSortedArray.java
+++ leetcode/hwarim/week3/SearchInRotatedSortedArray.java
@@ -1,15 +1,38 @@
 class SearchInRotatedSortedArray{
     public int search(int[] nums, int target) {
-        int length=nums.length,start,targetIndex;
-        if(length<1) return -1;
-        int[] nums2= nums.clone();
-        Arrays.sort(nums2);
-        targetIndex=Arrays.binarySearch(nums2,target);
+        int start,targetIndex;
+        
+        if(nums.length<1) return -1;
+
+        start=searchStart(nums,0,nums.length);
+        
+        targetIndex=Arrays.binarySearch(nums,0,start+1,target);
+        
+        if(0<=targetIndex) return targetIndex;
+        
+        targetIndex=Arrays.binarySearch(nums,start+1,nums.length,target);
+
         if(targetIndex<0) return -1;
-        start=Arrays.binarySearch(nums2,nums[0]);
-               
-        if(targetIndex<start) return length-start+targetIndex;
-        else return targetIndex-start;
+                       
+        return targetIndex;
        
+    }
+    
+    public int searchStart(int[] nums,int left,int right){
+        int middle,start=-1;
+        middle=left+(right-left)/2;
+        if(middle<nums.length-1&&nums[middle]>nums[middle+1]) return middle;  
+   
+        if(left<middle){
+            start=searchStart(nums,left,middle); 
+            if(start!=-1) return start;
+        }
+        
+        if(middle+1<right){
+            start=searchStart(nums,middle+1,right); 
+            if(start!=-1) return start;
+        }
+        
+        return -1 ;
     }
 }
