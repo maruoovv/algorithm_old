6a780a76864b4ca900c12951fea34038dda4ad6f
maruoovv <37106689+maruoovv@users.noreply.github.com>
2019-11-19 07:44:39 +0900

    Create LinkedListCycleII.java

---
+++ leetcode/sim/week9/LinkedListCycleII.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head ==  null || head.next == null) return null;
+        
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if (fast == null || fast.next == null) return null;
+            
+            if (slow == fast) break;
+        }
+        
+        slow = head;
+        // fast = fast.next.next;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        
+       return slow;
+    }
+}
